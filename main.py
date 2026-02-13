4c157d46e40c5105ec26799ff610b99f4b87867c
Avelile Mnyazana <avmnycpt025>
2026-02-14 00:38:12 +0200

    initial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json #WHY this is here? I don't see it being used anywhere in the code. Maybe it's for future use.
 with open('config.json') as f:
     urls = f.read().splitlines() # This reads the config.json file and splits it into a list of URLs, which will be used for monitoring the health of these APIs. Each URL is expected to be on a new line in the config.json file.
+    
     for url in urls:
         print(url)
